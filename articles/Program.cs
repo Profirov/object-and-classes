23cd05e232989b6490a6ecd0c0d317c7ff84dd03
user <user@DESKTOP-Q7SRQOE>
2023-12-01 18:39:21 +0200

    Add project files.

---
+++ articles/Program.cs
@@ -0,0 +1,33 @@
+﻿string articleData = Console.ReadLine();
+
+string title = articleData.Split(", ")[0];
+string content = articleData.Split(", ")[1];
+string author = articleData.Split(", ")[2];
+
+Article article = new Article(title, content, author);
+
+int countCommands = int.Parse(Console.ReadLine());
+for (int i = 1; i <= countCommands; i++)
+{ 
+  string command = Console.ReadLine();
+
+    string[] commandParts = command.Split(": ");
+    string commandName = commandParts[0];
+    string commandParameter = commandParts[1];
+
+    switch (commandName)
+    {
+        case "Edit":
+            article.Edit(commandParameter);
+            break;
+        case "ChangeAuthor":
+            article.ChangeAuthor(commandParameter);
+            break;
+        case "Rename":
+            article.Rename(commandParameter);
+            break;
+    }
+       
+}
+
+Console.WriteLine(article.ToString());
---
+++ sudents/Program.cs
@@ -0,0 +1,21 @@
+﻿int countStudent = int.Parse(Console.ReadLine());
+List<Student>students = new List<Student>();
+
+for (int count = 1; count <= countStudent; count++)
+{ 
+    String data = Console.ReadLine();
+    string[] dataParts = data.Split(" ");
+    string firstName = dataParts[0];
+    string lastName = dataParts[1];
+     double grade = double.Parse(dataParts[2]);
+
+    Student student = new Student(firstName, lastName, grade);
+    students.Add(student);
+}
+
+students = students.OrderByDescending(s => s.Grade).ToList();
+
+foreach (Student student in students)
+{
+    Console.WriteLine($"{student.FirstName} {student.LastName}: {student.Grade:F2}");
+}
